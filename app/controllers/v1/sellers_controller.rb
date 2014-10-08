a698682fdd99feb318cfd18444c72f3c0d5062f6
Vedant Agarwala <vedant.kota@gmail.com>
2014-10-08 12:44:29 +0530

    Added a sign_in helper

--- app/controllers/v1/sellers_controller.rb
+++ app/controllers/v1/sellers_controller.rb
@@ -20,6 +20,12 @@ class V1::SellersController < ApplicationController
     render json: { exists: V1::Seller.exists?(:email => params.require(:email)) }
   end
 
+  def sign_in
+    seller = V1::Seller.find_by(:email => authenticate_params.require(:email))
+    authenticated = seller.authenticate(authenticate_params.require(:password))
+    render json: authenticated
+  end
+
   # POST /v1/sellers
   # POST /v1/sellers.json
   def create
@@ -63,6 +69,10 @@ private
 
   def seller_params # TODO make this stronger to require instead of permit
     params.require(:seller).permit(:name_of_shop, :name_of_seller, :latitude, :longitude, :address, :phone, :email, :password, :product_categories => [])
+  end
+
+  def authenticate_params
+    params.require(:sign_in).permit(:email, :password)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :v1, defaults: {format: 'json'} do
     resources :sellers # secure this. as of now :new, :edit work without any authentication
     post 'sellers/exists'
+    post 'sellers/sign_in'
 
     resources :quotations, except: [:new, :edit]
     resources :devices, except: [:new, :edit]
