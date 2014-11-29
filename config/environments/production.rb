9f1d6d4e0b58f5491e0cd6ba3365d0fe98deba56
Vedant Agarwala <vedant.kota@gmail.com>
2014-11-30 03:23:39 +0530

    modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {:host => 'instano.in'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
+    :tls => true
     :address => "smtp.instano.in",
     :port => 587,
     :domain => "instano.in",
