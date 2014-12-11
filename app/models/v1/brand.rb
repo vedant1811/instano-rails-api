797fda0f40d1aa0f43c3721f5446431ffa383ab3
Vedant Agarwala <vedant.kota@gmail.com>
2014-12-11 18:19:15 +0530

    fixed regression of: 	Could not find the inverse association for category_name (:brand_name in V1::CategoryName) 	  app/serializers/v1/category_name_serializer.rb:10:in `brands'

--- app/models/v1/brand.rb
+++ app/models/v1/brand.rb
@@ -1,6 +1,6 @@
 class V1::Brand < ActiveRecord::Base
-  belongs_to :brand_name, :class_name => 'V1::BrandName', inverse_of: :brand
-  belongs_to :category, :class_name => 'V1::Category', inverse_of: :brand
+  belongs_to :brand_name, :class_name => 'V1::BrandName'
+  belongs_to :category, :class_name => 'V1::Category'
 
   validates :category, :uniqueness => {:scope => [:brand_name, :category]}
 end
--- app/models/v1/category_name.rb
+++ app/models/v1/category_name.rb
@@ -1,10 +1,10 @@
 class V1::CategoryName < ActiveRecord::Base
-  has_many :categories, :class_name => 'V1::Category', dependent: :delete_all, inverse_of: :category_name
+  has_many :categories, :class_name => 'V1::Category', dependent: :delete_all
   has_many :brand_names, :class_name => 'V1::BrandName', dependent: :delete_all, inverse_of: :category_name
 
   validates :name, :uniqueness => true
 
   # TODO: add validation for variants uniqueness and lower case
-
+  
   accepts_nested_attributes_for :brand_names, allow_destroy: true
 end
