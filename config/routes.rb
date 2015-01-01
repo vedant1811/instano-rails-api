09902655e7077a0efb2537be1f9992e6d2cc4537
Vedant Agarwala <vedant.kota@gmail.com>
2015-01-01 20:23:57 +0530

    Added config for devise and unable to sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :admin_users
   namespace :v1, defaults: {format: 'json'} do
     resources :sellers # secure this. as of now :new, :edit work without any authentication
     post 'sellers/exists'
