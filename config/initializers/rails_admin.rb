fb2c6a07f85b754ffaf955b370cb4b2676062303
Vedant Agarwala <vedant.kota@gmail.com>
2015-01-02 12:19:10 +0530

    Rails admin working at /newadmin (With hack of using active admin login) added config for supporting enums

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,3 +1,26 @@
+# see https://gist.github.com/dmilisic/38fcd407044ace7514df
+
+module ActiveRecord
+  module RailsAdminEnum
+    def enum(definitions)
+      super
+
+      definitions.each do |name, values|
+        define_method("#{ name }_enum") { self.class.send(name.to_s.pluralize).to_a }
+
+        define_method("#{ name }=") do |value|
+          if value.kind_of?(String) and value.to_i.to_s == value
+            super value.to_i
+          else
+            super value
+          end
+        end
+      end
+    end
+  end
+end
+ActiveRecord::Base.send(:extend, ActiveRecord::RailsAdminEnum)
+
 RailsAdmin.config do |config|
 
   ### Popular gems integration
