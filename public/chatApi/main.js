0ecc3298fbafce93329c2e29e0ad17e0ac259999
Vedant Agarwala <vedant.kota@gmail.com>
2015-02-11 19:40:31 +0530

    Added conig for keepalive

--- public/chatApi/main.js
+++ public/chatApi/main.js
@@ -2,14 +2,16 @@ require(['converse'], function (converse) {
   converse.initialize({
     auto_list_rooms: false,
     auto_subscribe: true,
-    bosh_service_url: 'https://bind.conversejs.org', // Please use this connection manager only for testing purposes
+    bosh_service_url: 'http://webchat.chatme.im/http-bind/', // Please use this connection manager only for testing purposes
     hide_muc_server: false,
     i18n: locales.en, // Refer to ./locale/locales.js to see which locales are supported
     prebind: false,
-    show_controlbox_by_default: true,
-    roster_groups: true
-    debug: true
+    keepalive: true,
+    show_controlbox_by_default: false,
+    roster_groups: true,
+    debug: true,
     // In order to keep all IM clients for a user engaged in a conversation, outbound messages are carbon-copied to all interested resources.
-    message_carbons: true
+    message_carbons: true,
+    play_sounds: true
   });
 });
