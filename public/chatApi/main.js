4d00ff3c35cd1b6a1a5d523802a8ad0b3ad0a972
Vedant Agarwala <vedant.kota@gmail.com>
2015-02-16 13:42:46 +0530

    added button with onClick in chatApi

--- public/chatApi/main.js
+++ public/chatApi/main.js
@@ -1,8 +1,12 @@
+function sellerClicked(seller_id) {
+  console.log("sellerClicked: " + seller_id);
+}
+
 require(['converse'], function (converse) {
   converse.initialize({
     auto_list_rooms: false,
     auto_subscribe: true,
-    bosh_service_url: 'http://webchat.chatme.im/http-bind/', // Please use this connection manager only for testing purposes
+    bosh_service_url: 'http://webchat.chatme.im/http-bind/',
     hide_muc_server: false,
     i18n: locales.en, // Refer to ./locale/locales.js to see which locales are supported
     prebind: false,
@@ -13,5 +17,9 @@ require(['converse'], function (converse) {
     // In order to keep all IM clients for a user engaged in a conversation, outbound messages are carbon-copied to all interested resources.
     message_carbons: true,
     play_sounds: true
+  });
+  // means we have a new user
+  converse.listen.on('noResumeableSession', function () {
+
   });
 });
