c042a51add1a6d5a068d1b82ceb2597a21bddb20
Vedant Agarwala <vedant.kota@gmail.com>
2015-02-18 11:49:53 +0530

    modified:   public/chatApi/main.js 	new file:   public/chatApi/sellerchat.html

--- public/chatApi/main.js
+++ public/chatApi/main.js
@@ -6,6 +6,7 @@ require(['converse'], function (converse) {
   converse.initialize({
     auto_list_rooms: false,
     auto_subscribe: true,
+    allow_registration: false,
     bosh_service_url: 'http://webchat.chatme.im/http-bind/',
     hide_muc_server: false,
     i18n: locales.en, // Refer to ./locale/locales.js to see which locales are supported
@@ -23,3 +24,7 @@ require(['converse'], function (converse) {
 
   });
 });
+
+/*
+"<query xmlns="jabber:iq:register"><instructions>Choose a username and password for use with this service.</instructions><username/><password/><x xmlns="jabber:x:data" type="form"><title>Creating a new account</title><instructions>Choose a username and password for use with this service.</instructions><field type="text-single" var="username" label="Username"><required/></field><field type="text-private" var="password" label="Password"><required/></field></x></query>"
+*/
