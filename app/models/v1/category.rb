dccf44676fbadb32b57e2e8f293abe554098e060
Vedant Agarwala <vedant.kota@gmail.com>
2015-02-19 01:35:22 +0530

    fixed bug of category name being deleted when seller is deleted

--- app/models/v1/category.rb
+++ app/models/v1/category.rb
@@ -1,6 +1,6 @@
 class V1::Category < ActiveRecord::Base
-  belongs_to :category_name, :class_name => 'V1::CategoryName', dependent: :delete
-  belongs_to :seller, :class_name => 'V1::Seller', dependent: :delete
+  belongs_to :category_name, :class_name => 'V1::CategoryName'
+  belongs_to :seller, :class_name => 'V1::Seller'
   has_many :brands, :class_name => 'V1::Brand', dependent: :delete_all
   has_many :brand_names, :class_name => 'V1::BrandName', through: :brands
 
