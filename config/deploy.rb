871d0a453e937af6ff8a1be7bd44cfa003f7aeff
Vedant Agarwala <vedant.kota@gmail.com>
2015-02-28 15:16:47 +0530

    updated production server IP

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ require "bundler/capistrano"
 require "rvm/capistrano"
 
 task :production do
-  server "ec2-52-1-14-239.compute-1.amazonaws.com", :app, :web, :db, :primary => true #ip of the server
+  server "54.85.8.97", :app, :web, :db, :primary => true #ip of the server
 end
 
 task :staging do
