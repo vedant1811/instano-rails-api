0f4b79e158e00706034c4afa80ad16fff9bf4b98
Vedant Agarwala <vedant.kota@gmail.com>
2015-03-01 17:08:12 +0530

    fixed bug: internal server error if a brand is orphaned (has no corresponding brand name)

--- app/serializers/v1/category_serializer.rb
+++ app/serializers/v1/category_serializer.rb
@@ -8,7 +8,7 @@ class V1::CategorySerializer < ActiveModel::Serializer
   def brands
     brand_names = []
     object.brands.each do |brand|
-      brand_names << brand.brand_name[:name]
+      brand_names << brand.brand_name[:name] unless brand.brand_name.nil?
     end unless object.brands.nil?
     brand_names
   end
