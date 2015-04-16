2892385623e0e4a692ae3e8108e0e649544e34fd
Vedant Agarwala <vedant.kota@gmail.com>
2015-04-16 19:44:45 +0530

    added error checking 	modified:   app/serializers/v1/brand_serializer.rb

--- app/serializers/v1/brand_serializer.rb
+++ app/serializers/v1/brand_serializer.rb
@@ -2,10 +2,18 @@ class V1::BrandSerializer < ActiveModel::Serializer
   attributes :name, :category
 
   def name
-    object.brand_name.name
+    if object.brand_name
+      object.brand_name.name
+    else
+      nil
+    end
   end
 
   def category
-    object.brand_name.category_name.name
+    if object.brand_name && object.brand_name.category_name
+      object.brand_name.category_name.name
+    else
+      nil
+    end
   end
 end
