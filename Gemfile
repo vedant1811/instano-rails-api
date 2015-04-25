a9a3bbf3054ea00b4a563e5ee8702a5cb84830fe
Vedant Agarwala <vedant.kota@gmail.com>
2015-04-25 19:27:19 +0530

    Started using ruby mine

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ gem 'rails', '~>4.2'
 gem 'pg'
 
 # To use ActiveModel has_secure_password
-gem 'bcrypt-ruby', '~> 3.1.2'
+gem 'bcrypt', '~> 3.1.2'
 
 # Use unicorn as the app server
 gem 'unicorn'
--- app/models/v1/category_name.rb
+++ app/models/v1/category_name.rb
@@ -1,6 +1,6 @@
 class V1::CategoryName < ActiveRecord::Base
   has_many :brand_names, :class_name => 'V1::BrandName', dependent: :destroy, inverse_of: :category_name
-  has_many :products, :class_name => 'V1::Product', dependent: :nullify
+  has_many :products, :class_name => 'V1::Product', through: :brand_names, dependent: :nullify
 #   accepts_nested_attributes_for :brand_names, allow_destroy: true
 
   validates :name, :uniqueness => {:case_sensitive => false}
